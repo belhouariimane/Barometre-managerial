9e9fd6bd36b79fd5370f90b5a53bc053c15163b4
Harboul <abdessabourharboul@gmail.com>
2020-05-25 10:27:35 +0200

    <AHA> configurer le CORS  pour les routes REST

--- Back/barometre-managerial/src/main/java/fr/univ/angers/info/m2/acdi/bm/controllers/StatistiqueController.java
+++ Back/barometre-managerial/src/main/java/fr/univ/angers/info/m2/acdi/bm/controllers/StatistiqueController.java
@@ -3,7 +3,6 @@ package fr.univ.angers.info.m2.acdi.bm.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +16,7 @@ import io.swagger.annotations.ApiOperation;
 @Api("API pour la gestion des statistiques.")
 @RestController
 @RequestMapping("/statistique")
-@CrossOrigin( origins = {"http://localhost:4200"} )
+//@CrossOrigin( origins = {"http://localhost:4200"} )
 public class StatistiqueController {
 
 	@Autowired
