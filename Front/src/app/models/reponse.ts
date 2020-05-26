64f1ec2d1d921ea8d1667972fbf2032031f3093a
Thibaut <gaudin.thibaut@gmail.com>
2020-05-26 04:05:22 +0200

    Gestion de la participation après remplissage du questionnaire

--- Front/src/app/models/reponse.ts
+++ Front/src/app/models/reponse.ts
@@ -1,9 +1,15 @@
 export class Reponse {
+    // identifiant de la réponse
     id: number;
-    idUser: number;
+    // identifiant de la question
     idQuestion: number;
-    idProposition: number;
-    value: string;
+    // tableau contenant les identifiants des propositions sélectionnées par le participant
+    // concerne les questions à choix unique (radio), multiples (checkbox)
+    // et les questions de sélection (combobox)
+    idsProposition: number[];
+    // valeur textuelle saisie par le participant
+    // concerne les questions ouvertes et les dates
+    valeur: string;
 
     constructor() {}
 }
--- Front/src/app/questionnaire/questionnaire-answer/questionnaire-answer.component.ts
+++ Front/src/app/questionnaire/questionnaire-answer/questionnaire-answer.component.ts
@@ -3,12 +3,14 @@ import {Questionnaire} from '../../models/questionnaire';
 import {QuestionService} from '../../services/question.service';
 import {QuestionnaireService} from '../../services/questionnaire.service';
 import {ActivatedRoute, Router} from '@angular/router';
-import {first} from 'rxjs/operators';
-import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormArray, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
 import {AlertService} from '../../services/alert.service';
-import {ReponseService} from '../../services/reponse.service';
 import {Question} from '../../models/question';
 import {Proposition} from '../../models/proposition';
+import {Participant} from '../../models/participant';
+import {MatCheckboxChange, MatRadioChange, MatSelectChange} from '@angular/material';
+import {Reponse} from '../../models/reponse';
+import {ParticipantService} from '../../services/participant.service';
 
 @Component({
   selector: 'app-questionnaire-answer',
@@ -18,32 +20,46 @@ import {Proposition} from '../../models/proposition';
 export class QuestionnaireAnswerComponent implements OnInit {
   answerForm: FormGroup;
   public questionnaire: Questionnaire;
+  participant: Participant;
   submitted = false;
   loading = false;
   done = false;
   prenom: string;
-
+  anonymous: boolean;
 
   constructor(private formBuilder: FormBuilder,
               private questionService: QuestionService,
               private questionnaireService: QuestionnaireService,
+              private participantService: ParticipantService,
               private alertService: AlertService,
-              private reponseService: ReponseService,
               private route: ActivatedRoute,
               private router: Router) {}
 
   ngOnInit(): void {
-    this.answerForm = this.formBuilder.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      questions: this.formBuilder.array([])
-    });
 
     // permet la redirection en cas d'identifiant inexistant
     this.questionnaireService.getById(this.route.snapshot.params.id)
         .subscribe(questionnaire => {
           if (questionnaire !== null) {
             this.questionnaire = questionnaire;
+            this.participant = new Participant();
+            this.participant.idQuestionnaire = questionnaire.id;
+            this.anonymous = questionnaire.anonymous;
+            if (this.anonymous) {
+              this.answerForm = this.formBuilder.group({
+                firstName: [''],
+                lastName: [''],
+                questions: this.formBuilder.array([])
+              });
+              // }, { validators: this.checkAnswersValidator});
+            } else {
+              this.answerForm = this.formBuilder.group({
+                firstName: ['', Validators.required],
+                lastName: ['', Validators.required],
+                questions: this.formBuilder.array([])
+              });
+              // }, { validators: this.checkAnswers});
+            }
           } else {
             this.alertService.clear();
             this.alertService.error('Le questionnaire demandé n\'existe pas.', true);
@@ -61,6 +77,39 @@ export class QuestionnaireAnswerComponent implements OnInit {
             });
           });
         });
+  }
+
+  checkAnswersValidator: ValidatorFn = (group: FormGroup): ValidationErrors | null => {
+    return this.checkAnswers(group) === true ? null : { 'requiredAnswers': true };
+  }
+
+  checkAnswers(group: FormGroup): boolean {
+    this.getQuestions(group).forEach((item) => {
+      if (item.value.isRequired) {
+        console.log('required : ' + item.value.valeurQuestion);
+        if (item.value.typeQuestion === 'OUVERT' || item.value.typeQuestion === 'DATE') {
+          if (item.value.valeurReponse === '') {
+            console.log('Erreur : ' + item.value.valeurQuestion);
+            return false;
+          }
+        } else {
+          let isRequiredOK = false;
+          this.getPropositions(item.value).forEach((prop) => {
+            // si on trouve au moins une valeur sélectionnée par le participant,
+            // on considère qu'il a répondu à la question obligatoire
+            if (prop.value.chosen && !isRequiredOK) {
+              isRequiredOK = true;
+            }
+          });
+          // sinon, on signale qu'aucune réponse n'a été détectée
+          if (!isRequiredOK) {
+            console.log('Erreur : ' + item.value.valeurQuestion);
+            return false;
+          }
+        }
+      }
+    });
+    return true;
   }
 
   initQuestion(question: Question) {
@@ -123,73 +172,60 @@ export class QuestionnaireAnswerComponent implements OnInit {
       return;
     }
 
-    this.prenom = this.answerForm.value.firstName;
-
-
-    this.answerForm.value.questions.forEach((item, index) => {
-      switch (item.typeQuestion) {
-        case 'OUVERT':
-        case 'DATE':
-          console.log('La réponse à la question : ' + item.valeurQuestion + ' est : ' + item.valeurReponse);
-          break;
-        case 'RADIO':
-        case 'COMBOBOX':
-        case 'CHECKBOX':
-          console.log(this.questions.get('0'));
-          console.log(this.questions.get('1'));
-          console.log(this.questions.get('2'));
-          console.log(this.questions.get('3'));
-          console.log(this.questions.get('propositions'));
-          console.log('La réponse à la question : ' + item.valeurQuestion + ' est : ' + item.id);
-          break;
-      }
-      console.log('index : ' + index);
-      console.log('reponse : ' + item.valeurReponse);
-      console.log('type : ' + item.typeQuestion);
-      console.log(this.questions.controls[index].value.valeurReponse);
-    });
-
     console.log(this.questions.controls[0].value.valeurReponse);
 
-    console.log(this.answerForm.value.textfin);
+    // récupère dans un premier temps le nom et prénom du participant si non anonyme
+    if (!this.anonymous) {
+      this.participant.prenom = this.answerForm.value.firstName;
+      this.participant.nom = this.answerForm.value.lastName;
+    }
 
-    // enregistre le nouvel utilisateur puis le connecte immédiatement
-    // this.loading = true;
-    // this.reponseService.create(this.answerForm.value)
-    //     .pipe(first())
-    //     .subscribe(
-    //         data => {
-    //           this.alertService.success('Participation enregistrée', true);
-    //           this.done = true;
-    //         }, error => {
-    //           this.alertService.error(error);
-    //           this.loading = false;
-    //         }
-    //     );
+    // puis, pour chaque question (même facultative et sans réponse)
+    // on insère dans le tableau de reponses la réponse du participant à la question
+    // cette réponse sera stockée soit comme chaine de caractères pour les questions ouvertes et de type date
+    // soit sous forme de tableau d'identifiant de propositions (préalablement enregistrées en base lors de la création du questionnaire)
+    this.participant.reponses = [];
+    this.getQuestions(this.answerForm).forEach((question) => {
+      const reponse = new Reponse();
+      reponse.idQuestion = question.value.idQuestion;
+      if (question.value.typeQuestion === 'OUVERT' || question.value.typeQuestion === 'DATE') {
+        reponse.valeur = question.value.valeurReponse;
+      } else {
+        reponse.idsProposition = [];
+        console.log('Question : ' + question.value.valeurQuestion);
+        console.log('Propositions : ');
+        this.getPropositions(question).forEach((proposition) => {
+          console.log(proposition.value.valeurProp + ' : ' + proposition.value.chosen);
+          if (proposition.value.chosen) {
+            reponse.idsProposition.push(proposition.value.idProp);
+          }
+        });
+      }
+      this.participant.reponses.push(reponse);
+    });
+
+    // lorsque l'objet participant est complet, on l'envoie vers l'API
+    this.participantService.create(this.participant).subscribe();
     this.done = true;
   }
 
-  onChangeSelect(event: any, index: number) {
-    console.log(event.value);
-    this.questions.controls[index].value.valeurReponse = event.value;
-    console.log(this.questions.controls[index]);
-    console.log('SELECT');
+  onChangeSelect(event: any, question: Question) {
+    this.getPropositions(question).forEach((item, index) => {
+      if (item.value.valeurProp === this.getPropositions(question)[event.value].value.valeurProp) {
+        item.value.chosen = true;
+      } else {
+        item.value.chosen = false;
+      }
+      console.log(item.value.valeurProp + ' : ' + item.value.chosen);
+    });
   }
 
-  onChangeCheckbox(values: any, question: Question,  proposition: Proposition) {
-    console.log('values: ' + values.currentTarget.id);
-    console.log('valuesIndex: ' + values.currentIndex);
-    console.log('à changer : ' + this.getPropositions(question)[values.currentTarget.id].controls.chosen);
-    console.log('à changer : ' + this.getPropositions(question)[values.currentTarget.id].value.chosen);
-    console.log('à changer : ' + this.getPropositions(question)[values.currentTarget.id].chosen);
-    console.log('Nouvelle valeur : ' + values.currentTarget.checked);
-    console.log(this.getPropositions(question).forEach((item) => {
-      console.log(item.value.valeurProp + ' = ? ' + proposition.valeur);
-      if (item.value.valeurProp === proposition.valeur) {
-        console.log('checked or not? : ' + values.currentTarget.checked);
-        item.chosen = values.currentTarget.checked;
-        console.log(item.chosen);
+  onChangeCheckbox(event: MatCheckboxChange, question: Question) {
+    this.getPropositions(question).forEach((item) => {
+      if (item.value.valeurProp === this.getPropositions(question)[event.source.id].value.valeurProp) {
+        item.value.chosen = event.checked;
+        console.log(item.value.valeurProp + ' : ' + item.value.chosen);
       }
-    }));
+    });
   }
 }
