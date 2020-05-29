b23699b1a749e45ad8b1f408158f957b10fa8520
belhouariimane <belhouari.imane@gmail.com>
2020-05-29 02:57:14 +0200

    update route pour faire fonctionner le serveur
    sur le local on utilise le proxy , mais sur le serveur c'est le env.prod.ts qui est utilisé

--- Front/src/app/services/participant.service.ts
+++ Front/src/app/services/participant.service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Participant} from '../models/participant';
 import {map} from 'rxjs/operators';
 import {Observable} from 'rxjs';
+import {environment} from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class ParticipantService {
@@ -11,11 +12,11 @@ export class ParticipantService {
 
     create(participant: Participant) {
         const headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
-        return this.http.post(`/public/participant/create`, JSON.stringify(participant), {headers});
+        return this.http.post(`${environment.apiUrl}/public/participant/create`, JSON.stringify(participant), {headers});
     }
 
     readAllByIdQuestionnaire(idQuestionnaire: number): Observable<Participant[]> {
-        return this.http.get<any>(`/participant/readAll`)
+        return this.http.get<any>(`${environment.apiUrl}/participant/readAll`)
             .pipe(map(data => {
                return data.retour.filter(x => x.idQuestionnaire === idQuestionnaire);
             }));
